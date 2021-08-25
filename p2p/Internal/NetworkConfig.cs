020880a5e4a13ab54c154742f6731791ac14b86e
Krobelus <krobelus@irinabot.ru>
2021-08-25 10:10:49 +0300

    Добавлены конфиги для PrivateNetwork и RemoteClient

---
+++ p2p/Internal/NetworkConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2P.Internal
+{
+    internal class NetworkConfig
+    {
+        private int maxHelloAttemts  = 30;
+
+        private int pingInterval = 5000;
+
+        private int helloInterval = 1000;
+
+        public int MaxHelloAttemts { get => maxHelloAttemts; set => maxHelloAttemts = value; }
+        public int PingInterval { get => pingInterval; set => pingInterval = value; }
+        public int HelloInterval { get => helloInterval; set => helloInterval = value; }
+    }
+}
---
+++ p2p/Internal/PeerConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2P.Internal
+{
+    internal class PeerConfig
+    {
+        private bool preferTcp;
+
+        public bool PreferTcp { get => preferTcp; set => preferTcp = value; }
+    }
+}
